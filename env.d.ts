16e5241a48ff6bd290e6da77e70cba39f235142f
koralle <koralle@koralle-mgmg.com>
2025-11-23 02:24:33 +0900

    feat: introduce Biome (#3)
    * chore(deps): add Biome
    
    * feat: use Biome instead of Prettier

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    NODE_ENV: 'development' | 'production';
+    NODE_ENV: "development" | "production";
     npm_package_version: `${number}.${number}.${number}`;
   }
 }
